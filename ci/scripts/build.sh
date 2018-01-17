47b6b9e17fdcfe605899e382a92aff4e5757a835
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-01-17 18:00:12 -0400

    ci(build): fix duplicate commits

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
+git checkout "$TRAVIS_BRANCH"
 yarn release -t ''
 export GIT_TAG=$(jq -r ".version" package.json)
 git reset HEAD^1
 
 yarn build 
 
-if [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TRAVIS_COMMIT_MESSAGE" != *"ci(build): update library to version "* ]
+if [ "$TRAVIS_BRANCH" = "feature/register-user" ] && [ "$TRAVIS_COMMIT_MESSAGE" != *"ci(build): update library to version "* ]
 then
-    git checkout "$TRAVIS_BRANCH"
     git add lib/
     git commit -m "ci(build): update library to version ${GIT_TAG}"
     git push origin "$TRAVIS_BRANCH"
